e9f696f7b03fd2b9e1db3de35f4a49d5ed81674e
Fernando <FernandoAguilar984@gmail.com>
2023-10-05 02:55:54 -0600

    add unique constraint to schema

--- lib/stockprices_chat/schemas/stockprice_record.ex
+++ lib/stockprices_chat/schemas/stockprice_record.ex
@@ -12,5 +12,6 @@ defmodule StockpricesChat.StockpriceRecord do
     struct
     |> cast(params, [:ticker, :date, :price])
     |> validate_required([:ticker, :date, :price])
+    |> unique_constraint(:stockprices_ticker_date_index)
   end
 end
